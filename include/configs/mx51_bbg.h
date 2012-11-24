286c299beee2f046a7d8e6aa87defbcfe61267f3
Ricardo Garcia <ricardogarcia_fido@hotmail.com>
2012-11-24 13:12:23 -0600

    Define added to enable Splash Screen.

--- include/configs/mx51_bbg.h
+++ include/configs/mx51_bbg.h
@@ -77,6 +77,8 @@
 #define CONFIG_BAUDRATE			115200
 #define CONFIG_SYS_BAUDRATE_TABLE	{9600, 19200, 38400, 57600, 115200}
 
+/* Enable Splash Screen */
+#define CONFIG_SPLASH_SCREEN 
 /***********************************************************
  * Command definition
  ***********************************************************/

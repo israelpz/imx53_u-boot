cb3dcb9835ab9f9341332c62e6fbbe4a9cd79ee9
ArminBalderas <balderas@gmx.net>
2012-11-24 12:55:06 -0600

    Cambio en los atributos de las secciones de memoria NAND. Signed-off-by: ArminBalderas <balderas@gmx.net>

--- board/freescale/mx53_loco/mx53_loco_.c
+++ board/freescale/mx53_loco/mx53_loco_.c
@@ -225,27 +225,9 @@ void board_mmu_init(void)
 	X_ARM_MMU_SECTION(0xF7F, 0xF7F, 0x001,
 			ARM_UNCACHEABLE, ARM_UNBUFFERABLE,
 			ARM_ACCESS_PERM_RW_RW); /* NAND Flash buffer */
-	X_ARM_MMU_SECTION(0xF7F, 0x050, 0x001,
-			ARM_CACHEABLE, ARM_BUFFERABLE,
-			ARM_ACCESS_PERM_RW_RW); /* NAND Flash buffer 1*/
-	X_ARM_MMU_SECTION(0xF7F, 0x0AA, 0x001,
-			ARM_UNCACHEABLE, ARM_UNBUFFERABLE,
-			ARM_ACCESS_PERM_RW_RW); /* NAND Flash buffer 2*/
 	X_ARM_MMU_SECTION(0xF80, 0xF80, 0x080,
 			ARM_UNCACHEABLE, ARM_UNBUFFERABLE,
 			ARM_ACCESS_PERM_RW_RW); /* iRam + GPU3D + Reserved */
-
-
-X_ARM_MMU_SECTION(0xB00, 0xff0, 0x1,
-                        ARM_CACHEABLE, ARM_BUFFERABLE,
-                        ARM_ACCESS_PERM_RO_RO);
-X_ARM_MMU_SECTION(0xB00, 0xff1, 0x1,
-                        ARM_CACHEABLE, ARM_BUFFERABLE,
-                        ARM_ACCESS_PERM_RW_RW);
-X_ARM_MMU_SECTION(0xB00, 0xff2, 0x1,
-                        ARM_CACHEABLE, ARM_BUFFERABLE,
-                        ARM_ACCESS_PERM_RW_RW);
-
 
 	/* Workaround for arm errata #709718 */
 	/* Setup PRRR so device is always mapped to non-shared */

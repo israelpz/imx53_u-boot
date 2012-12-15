cfb43362183ef0c9dbf6238e98d1ae3b0ea7c037
Ernesto Colina <netoxp@gmail.com>
2012-12-15 12:27:23 -0600

    Hello_World_asignacion 	new file:   hello_world/hello_proc/.hello_proc.ko.cmd 	new file:   hello_world/hello_proc/.hello_proc.mod.o.cmd 	new file:   hello_world/hello_proc/.hello_proc.o.cmd 	new file:   hello_world/hello_proc/.tmp_hello_proc.gcno 	new file:   hello_world/hello_proc/.tmp_versions/hello_proc.mod 	new file:   hello_world/hello_proc/Makefile 	new file:   hello_world/hello_proc/Module.symvers 	new file:   hello_world/hello_proc/hello_proc.c 	new file:   hello_world/hello_proc/hello_proc.ko 	new file:   hello_world/hello_proc/hello_proc.mod.c 	new file:   hello_world/hello_proc/modules.order

---
+++ hello_world/hello_proc/.hello_proc.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/cynetik/hello_world/hello_proc/hello_proc.ko := ld -r -m elf_i386 -T /home/cynetik/linux-3.2.17/scripts/module-common.lds --build-id  -o /home/cynetik/hello_world/hello_proc/hello_proc.ko /home/cynetik/hello_world/hello_proc/hello_proc.o /home/cynetik/hello_world/hello_proc/hello_proc.mod.o
---
+++ hello_world/hello_proc/.tmp_versions/hello_proc.mod
@@ -0,0 +1,2 @@
+/home/cynetik/hello_world/hello_proc/hello_proc.ko
+/home/cynetik/hello_world/hello_proc/hello_proc.o
